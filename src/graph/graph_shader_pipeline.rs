c92ec595ca5df080b5794d2a2f06d48c73b70e14
lrshsl <hoeslilars@gmail.com>
2025-07-21 08:25:27 +0200

    Use LoadOp::Load, such that not the whole screen is redrawn by the shader

--- src/graph/graph_shader_pipeline.rs
+++ src/graph/graph_shader_pipeline.rs
@@ -195,7 +195,7 @@ impl FragmentShaderPipeline {
                 view: target,
                 resolve_target: None,
                 ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                    load: wgpu::LoadOp::Load,
                     store: wgpu::StoreOp::Store,
                 },
             })],
