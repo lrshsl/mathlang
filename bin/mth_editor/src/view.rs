592adfd0b399020912be831b870ce0c21040b006
lrshsl <hoeslilars@gmail.com>
2026-02-03 01:37:00 +0100

    Simplify UI

--- bin/mth_editor/src/view.rs
+++ bin/mth_editor/src/view.rs
@@ -8,48 +8,33 @@ use crate::{MainState, message::Message};
 
 impl MainState {
     pub fn view(&'_ self) -> Element<'_, Message> {
-        column![
-            container(text("Title").size(30)).center_x(Fill),
-            row![
-                column![].width(50),
-                self.text_editor_view(),
-                space().height(Fill),
-                self.graph_view(),
-                column![].width(50)
-            ]
+        row![
+            container(self.text_editor_view()).width(FillPortion(30)),
+            container(self.graph_view()).width(FillPortion(70))
         ]
-        .height(Fill)
+        .padding(50)
         .into()
     }
 
     fn text_editor_view(&'_ self) -> Element<'_, Message> {
         column![
-            text("Editor").size(30).height(FillPortion(6)),
             container(
                 widget::text_editor(&self.text)
                     .size(30)
                     .height(Fill)
                     .on_action(Message::EditText)
             )
-            .width(FillPortion(30))
-            .height(FillPortion(90))
+            .height(FillPortion(80))
             .style(container::rounded_box),
-            row![widget::text(
+            container(widget::text(
                 self.err_msg.clone().unwrap_or("No errors".to_string())
-            )]
+            ))
             .height(FillPortion(20)),
-            row![].height(FillPortion(4)),
         ]
         .into()
     }
 
     fn graph_view(&'_ self) -> Element<'_, Message> {
-        column![
-            container(widget::shader(&self.graph).height(Fill).width(Fill))
-                .style(container::rounded_box)
-                .height(Fill)
-                .width(FillPortion(70)),
-        ]
-        .into()
+        widget::shader(&self.graph).height(Fill).width(Fill).into()
     }
 }
